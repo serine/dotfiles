0406f7541b39d7e9c6666337b5fc2dac3ccd5faa
Kiril Tsyganov <kirill.tsyganov@monash.edu>
2015-12-21 17:53:48 +1100

    quick update to prompt.bash file

--- bash/.bashrc.d/prompt.bash
+++ bash/.bashrc.d/prompt.bash
@@ -9,8 +9,8 @@ case "$TERM" in
 esac
 
 #----------------------------------------------------------------------------------------------------
-if [ "$color_prompt" = yes ]; then
-    if git --version &>/dev/null; then
+if [[ "$color_prompt" = yes ]]; then
+    if [[ `git --version` =~ "1.9" ]]; then
         # PS1 Line to show current Git Branch in the Prompt
 	PS1='${debian_chroot:+($debian_chroot)}\[\033[1;36m\][\h]\[\033[00m\]\w\[\033[1;32m\]$(__git_ps1)\[\033[00m\]\$ '
     else
@@ -18,7 +18,7 @@ if [ "$color_prompt" = yes ]; then
         PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
     fi
 else
-    if git --version &>/dev/null; then
+    if [[ `git --version` =~ "1.9" ]]; then
         # PS1 Line to show current Git Branch in the Prompt
         PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w $(__git_ps1 "(%s)")\$ '
     else
