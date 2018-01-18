dc877178090d86edcb64c611f70e6503e20068e1
serine <tskiril@gmail.com>
2018-01-19 09:43:03 +1100

    updated aliases

--- bash/.bashrc.d/aliases.bash
+++ bash/.bashrc.d/aliases.bash
@@ -1,11 +1,9 @@
-#-------------------------
 # Set your alieases here
-#-------------------------
 
 alias ll='ls -alF'
 alias la='ls -lha'
 alias l='ls -CF'
 alias lr="ls -1 --color"
-alias ld="ls -ld .*"
-alias go='gnome-open'
 alias c='clear'
+alias grep="grep --color-always"
+alias zgrep="zgrep --color-always"
