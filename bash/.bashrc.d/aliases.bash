0142fc6a2073e45c4e8e90932103b64a5509e930
Kirill <kirill.tsyganov@monash.edu>
2018-03-19 10:56:04 +0000

    updated bashrc aliases

--- bash/.bashrc.d/aliases.bash
+++ bash/.bashrc.d/aliases.bash
@@ -5,5 +5,5 @@ alias la='ls -lha'
 alias l='ls -CF'
 alias lr="ls -1 --color"
 alias c='clear'
-alias grep="grep --color-always"
-alias zgrep="zgrep --color-always"
+alias grep="grep --color=always"
+alias zgrep="zgrep --color=always"
