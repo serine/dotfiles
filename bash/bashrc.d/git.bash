c087b7ee97c4bae8f5ab4e51d6bdd64f1eda7f55
serine <tskiril@gmail.com>
2018-05-31 11:19:48 +1000

    Fixed issues with bash completion, last diff types of tab completions all sorted now. Also turned off bell-sound on tab completion

---
+++ bash/bashrc.d/git.bash
@@ -0,0 +1,8 @@
+# enable git prompt and bash tab completion
+if [ -f /opt/git-2.17.1/contrib/completion/git-prompt.sh ]
+then
+
+  . /opt/git-2.17.1/contrib/completion/git-prompt.sh
+  . /opt/git-2.17.1/contrib/completion/git-completion.bash
+
+fi
