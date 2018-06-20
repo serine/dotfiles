f9b58a9495375135105c38afcfb2329e269dbf79
serine <tskiril@gmail.com>
2018-06-20 22:57:03 +1000

    added a couple of handy aliases to my bash

--- bash/bashrc.d/aliases.bash
+++ bash/bashrc.d/aliases.bash
@@ -4,6 +4,10 @@ alias ll='ls -alF'
 alias la='ls -lha'
 alias l='ls -CF'
 alias lr="ls -1 --color"
+#more info here https://unix.stackexchange.com/questions/120077/the-ls-command-is-not-working-for-a-directory-with-a-huge-number-of-files
+alias lshuge="ls --sort=none"
 alias c='clear'
 alias grep="grep --color=always"
 alias zgrep="zgrep --color=always"
+alias do-open="xdg-open"
+alias ssh-with-pass="ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no"
