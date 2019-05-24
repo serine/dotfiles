c5ada86f0fc0b1829c1110991109c0ebe7cb9747
serine <tskiril@gmail.com>
2019-05-24 11:23:17 +1000

    updates

--- bash/bashrc.d/git.bash
+++ bash/bashrc.d/git.bash
@@ -1,8 +1,9 @@
 # enable git prompt and bash tab completion
-if [ -f /opt/git-2.17.1/contrib/completion/git-prompt.sh ]
+if [[ -f /opt/git-2.17.1/contrib/completion/git-prompt.sh ]]
 then
-
-  . /opt/git-2.17.1/contrib/completion/git-prompt.sh
-  . /opt/git-2.17.1/contrib/completion/git-completion.bash
-
+  source /opt/git-2.17.1/contrib/completion/git-prompt.sh
+  source /opt/git-2.17.1/contrib/completion/git-completion.bash
+elif [[ -f /usr/share/git-core/contrib/completion/git-prompt.sh ]]
+then
+  source /usr/share/git-core/contrib/completion/git-prompt.sh
 fi
